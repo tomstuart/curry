54814e20208f8b87f43d7d6f23694280520a49b6
Tom Stuart <tom@codon.com>
2014-08-31 22:21:38 +0100

    Check for too many arguments at currying time

--- lib/curry.rb
+++ lib/curry.rb
@@ -2,13 +2,15 @@ class Proc
   remove_method :curry
 
   def curry(arity = nil)
-    number_of_args = self.arity < 0 ? -self.arity - 1 : self.arity
+    has_optional_args = self.arity < 0
+    number_of_args = has_optional_args ? -self.arity - 1 : self.arity
     arity ||= number_of_args
 
     if lambda?
       too_few_args = arity < number_of_args
+      too_many_args = arity > number_of_args && !has_optional_args
 
-      if too_few_args
+      if too_few_args || too_many_args
         raise ArgumentError, "wrong number of arguments (#{arity} for #{number_of_args})"
       end
     end
