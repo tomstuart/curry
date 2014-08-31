c3f896fa0475dcee3f6dab68a46bcb282775d321
Tom Stuart <tom@codon.com>
2014-08-31 22:14:01 +0100

    Support optional arguments

--- lib/curry.rb
+++ lib/curry.rb
@@ -1,7 +1,9 @@
 class Proc
   remove_method :curry
 
-  def curry(arity = self.arity)
+  def curry(arity = nil)
+    arity ||= self.arity < 0 ? -self.arity - 1 : self.arity
+
     proc do |*args|
       if args.length < arity
         proc { |*more_args| call(*args + more_args) }.curry(arity - args.length)
